a5833636a0ba65e6184f8640a2e894adf18f2a77
paflov <philipp.franck@hu-berlin.de>
2025-01-16 13:30:58 +0100

    Include units in package

--- dev-tools/package-builder.js
+++ dev-tools/package-builder.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const projectPath = path.join(__dirname, '/../');
 const runtimesDirectoryPath = `${projectPath}CBAItemBuilderSupportedRuntimes/`;
+const runtimesDirectoryPath = `${projectPath}units/`;
 const distDir = `${projectPath}dist/`;
 
 const prepare = () => {
@@ -51,6 +52,48 @@ const collectRunTimeVersions = () => {
   return dependencies;
 };
 
+const collectUnits = () => {
+  const units = { };
+  let unitsDirContent = [];
+
+  try {
+    unitsDirContent = fs.readdirSync(runtimesDirectoryPath);
+  } catch (err) {
+    console.err('Error reading the directory:', err);
+    process.exit(1);
+  }
+
+  unitsDirContent.forEach(itemDir => {
+    if (itemDir.startsWith('.')) return;
+    // TODO unzip ?
+    const stats = fs.statSync(`${runtimesDirectoryPath}/${itemDir}`);
+    if (!stats.isDirectory()) return;
+    if (!fs.existsSync(`${runtimesDirectoryPath}/${itemDir}/config.json`)) {
+      console.log(`${itemDir}: config.json not found.`);
+      return;
+    }
+    let config = {};
+    try {
+      config = fs.readFileSync(`${runtimesDirectoryPath}/${itemDir}/config.json`).toJSON();
+    } catch (e) {
+      console.log(`${itemDir}: config.json could not be parsed.`);
+      console.warn(e);
+      return;
+    }
+    fs.cpSync(`${runtimesDirectoryPath}/${itemDir}`, `${distDir}/${itemDir}`, { recursive: true });
+    const untitDef = {
+      task: config.tasks[0].name,
+      page: config.tasks[0].initialPage,
+      scope: 'A',
+      runtimeVersion: config.runtimeCompatibilityVersion,
+      name: config.name
+    };
+    fs.writeFileSync(`${runtimesDirectoryPath}/${itemDir}.ib2verona.json`, JSON.stringify(untitDef));
+    units[itemDir] = config;
+  });
+  return units;
+};
+
 const createPlayers = dependencies => {
   const playerFile = fs.readFileSync(`${projectPath}/src/ib-runtime.template.html`, 'utf8');
   Object.entries(dependencies)
@@ -78,6 +121,8 @@ const build = () => {
   const dependencies = collectRunTimeVersions();
   console.log('[create index files]');
   createPlayers(dependencies);
+  console.log('[collect units]');
+  const units = collectUnits();
   console.log('[done]');
   console.log('\n');
 };
